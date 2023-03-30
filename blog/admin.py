e187976e72081aa0ee1284619e3a34a89ac3ea8b
Vadym Bihun <bihunva@gmail.com>
2023-03-30 13:31:06 +0300

    Added Post model and registered it in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from blog.models import User
+from blog.models import User, Post
 
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
     list_display = ['username', 'email', 'first_name', 'last_name']
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_filter = ['created', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'created'
+    ordering = ['created']
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,26 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="created")
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [models.Index(fields=['-created'])]
+
+    def __str__(self):
+        return self.title
